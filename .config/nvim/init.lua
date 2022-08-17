028887b22bce44c9cecadb6a95d1f782e3ff3455
Aleksandr Cherepanov <aleksandr.s.cherepanov@gmail.com>
2022-08-17 22:06:34 +0500

    add nvim-tree diagnostics

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,19 @@ require('global')
 require('editor')
 require('plugins')
 
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+	diagnostics = { 
+		enable = true,
+		show_on_dirs = false,
+		debounce_delay = 50, 
+		icons = {
+			hint = "", 
+			info = "", 
+			warning = "", 
+			error = "", 
+		}, 
+	}
+})
 
 require('keymaps')
 require('color')
